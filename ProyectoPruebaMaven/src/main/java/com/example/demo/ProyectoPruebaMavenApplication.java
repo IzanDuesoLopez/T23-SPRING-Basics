e450e0fb15c8835a81b5b48bd9b697894f158d43
IzanDuesoLopez <izandueso@gmail.com>
2022-08-09 11:57:46 +0200

    Proyecto Maven 1

---
+++ ProyectoPruebaMaven/src/main/java/com/example/demo/ProyectoPruebaMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPruebaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPruebaMavenApplication.class, args);
+	}
+
+}
