f823af99414556a71d82dc89257650c2ad1ad3cf
IzanDuesoLopez <izandueso@gmail.com>
2022-08-09 11:59:46 +0200

    Proyecto Graddle 1

---
+++ ProyectoPruebaGraddle/src/main/java/com/example/demo/ProyectoPruebaGraddleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPruebaGraddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPruebaGraddleApplication.class, args);
+	}
+
+}
